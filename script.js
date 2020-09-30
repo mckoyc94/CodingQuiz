a72a61a82702779930e1e2f658c03e1039435868
Cmac <mckoyc@gmail.com>
2020-09-30 11:38:03 -0400

    added buttons to questions

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var startButton = document.getElementById("start")
 var timerEl = document.getElementById("countDown")
 var questNum = 0
 
+var buttonSection = document.createElement("div")
+    var buttonA = document.createElement("button")
+    var buttonB = document.createElement("button")
+    var buttonC = document.createElement("button")
+    var buttonD = document.createElement("button")
+
 // Quiz Questions
 var questArray = [
     {
@@ -17,7 +23,8 @@ var questArray = [
         "answer1" : "<java>",
         "answer2" : "<script>",
         "answer3" : "<javascript>",
-        "answer4" : "<js>"
+        "answer4" : "<js>",
+        "correctAnswer" : "answer2"
     },
     // Question 2
     {
@@ -25,7 +32,8 @@ var questArray = [
         "answer1" : "getCharacterAt()",
         "answer2" : "getChar()",
         "answer3" : "charAt()",
-        "answer4" : "characterAt()"   
+        "answer4" : "characterAt()",
+        "correctAnswer" : "answer3"
     },
     // Question 3
     {
@@ -33,7 +41,8 @@ var questArray = [
         "answer1" : "<bold>",
         "answer2" : "<thick>",
         "answer3" : "<big>",
-        "answer4" : "<strong>"
+        "answer4" : "<strong>",
+        "correctAnswer" : "answer4"
     },
     // Question 4
     {
@@ -41,7 +50,8 @@ var questArray = [
         "answer1" : "<link>",
         "answer2" : "<merge>",
         "answer3" : "<attach>",
-        "answer4" : "<bound>"
+        "answer4" : "<bound>",
+        "correctAnswer" : "answer1"
     },
     // Question 5
     {
@@ -49,22 +59,23 @@ var questArray = [
         "answer1" : "string()",
         "answer2" : "stringMaker()",
         "answer3" : "toString()",
-        "answer4" : "subString()"
+        "answer4" : "subString()",
+        "correctAnswer" : "answer3"
     }
 
 ]
 
 //Answer Key
-questArray[1].answer2 = true;
-questArray[2].answer3 = true;
-questArray[3].answer4 = true;
-questArray[4].answer1 = true;
-questArray[5].answer3 = true;
+questArray[1].answer2
+questArray[2].answer3 
+questArray[3].answer4 
+questArray[4].answer1 
+questArray[5].answer3 
 
 
 // Timer
 function startTimer(){
-    var timer = 5;
+    var timer = 60;
 
     var timeInterval = setInterval(function() {
         timerEl.textContent = timer + " seconds remaining";
@@ -83,16 +94,38 @@ function startTimer(){
 // Start Quiz
 
 startButton.addEventListener("click", function(){
+    
     startButton.remove()
+    document.body.appendChild(buttonSection)
+    buttonSection.appendChild(buttonA)
+    buttonSection.appendChild(buttonB)
+    buttonSection.appendChild(buttonC)
+    buttonSection.appendChild(buttonD)
+
+
     nextQuestion()
     startTimer()
 })
 
+
 // Generating with Next Question
 function nextQuestion(){
+    var buttonEl = document.querySelector("button")
     questNum++
+    
     currentQuestionNumber.textContent = "Question #" + questNum;
     currentQuestion.textContent = questArray[questNum].text;
+    buttonA.textContent = questArray[questNum].answer1
+    buttonB.textContent = questArray[questNum].answer2
+    buttonC.textContent = questArray[questNum].answer3
+    buttonD.textContent = questArray[questNum].answer4
+  
+    buttonB.addEventListener('click', function(){
+        console.log("Button clicked")
+        console.log(buttonEl)
+    })
+
+
     
     
 
@@ -104,7 +137,8 @@ function nextQuestion(){
 function highScoreScreen(){
     currentQuestionNumber.textContent = "All done!"
     currentQuestion.textContent= "Nice Job! Enter your initials"
-          
+    buttonSection.remove()
+    
     var textForm = document.createElement("input")
     textForm.setAttribute("type", "text")
     document.body.append(textForm)
