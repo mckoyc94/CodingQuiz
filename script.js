d7e53c81ac34fd21dbe1607dd7d3a0f907576a50
Cmac <mckoyc@gmail.com>
2020-09-30 01:48:05 -0400

    added questions array to javascript

--- script.js
+++ script.js
@@ -4,7 +4,63 @@ var currentQuestionNumber = document.querySelector("h1")
 var currentQuestion = document.querySelector("p")
 var startButton = document.getElementById("start")
 var timerEl = document.getElementById("countDown")
-var questNum = 1
+var questNum = 0
+
+// Quiz Questions
+var questArray = [
+    {
+            "text": "blank"
+    },
+    // Question 1
+    {
+        "text" : "Inside which HTML element do we put the JavaScript?",
+        "answer1" : "<java>",
+        "answer2" : "<script>",
+        "answer3" : "<javascript>",
+        "answer4" : "<js>"
+    },
+    // Question 2
+    {
+        "text" : "Which built-in method returns the character at the specified index?",
+        "answer1" : "getCharacterAt()",
+        "answer2" : "getChar()",
+        "answer3" : "charAt()",
+        "answer4" : "characterAt()"   
+    },
+    // Question 3
+    {
+        "text" : "Which tag would you use to make something bold in HTML?",
+        "answer1" : "<bold>",
+        "answer2" : "<thick>",
+        "answer3" : "<big>",
+        "answer4" : "<strong>"
+    },
+    // Question 4
+    {
+        "text" : "How do you attach an external file to a HTML file in HTML?",
+        "answer1" : "<link>",
+        "answer2" : "<merge>",
+        "answer3" : "<attach>",
+        "answer4" : "<bound>"
+    },
+    // Question 5
+    {
+        "text" : "Which of the following function of String object returns a string representing the specified object?",
+        "answer1" : "string()",
+        "answer2" : "stringMaker()",
+        "answer3" : "toString()",
+        "answer4" : "subString()"
+    }
+
+]
+
+//Answer Key
+questArray[1].answer2 = true;
+questArray[2].answer3 = true;
+questArray[3].answer4 = true;
+questArray[4].answer1 = true;
+questArray[5].answer3 = true;
+
 
 // Timer
 function startTimer(){
@@ -15,7 +71,9 @@ function startTimer(){
         timer--;
     
         if (timer === 0 || questNum === 6) {
-          highScoreScreen()
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+            highScoreScreen()
         }
     
       }, 1000);
@@ -30,11 +88,11 @@ startButton.addEventListener("click", function(){
     startTimer()
 })
 
-// Quiz Questions
+// Generating with Next Question
 function nextQuestion(){
-
+    questNum++
     currentQuestionNumber.textContent = "Question #" + questNum;
-    currentQuestion.textContent = " ";
+    currentQuestion.textContent = questArray[questNum].text;
     
     
 
@@ -44,8 +102,6 @@ function nextQuestion(){
 
 // High Score
 function highScoreScreen(){
-    timerEl.textContent = "";
-    clearInterval(timeInterval);
     currentQuestionNumber.textContent = "All done!"
     currentQuestion.textContent= "Nice Job! Enter your initials"
           
@@ -53,5 +109,5 @@ function highScoreScreen(){
     textForm.setAttribute("type", "text")
     document.body.append(textForm)
 
-    
+
 }
