4372202b10e8ac504f4352db804411ae4ddd85d6
Cmac <mckoyc@gmail.com>
2020-09-30 00:21:35 -0400

    worked on timer code, moved code from timer to new function and had timer call function. Added JS tags to high score page

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
 // Variables Section
 var quizSection = document.querySelector("main")
-var currentQuestion = document.querySelector("h1")
+var currentQuestionNumber = document.querySelector("h1")
+var currentQuestion = document.querySelector("p")
 var startButton = document.getElementById("start")
 var timerEl = document.getElementById("countDown")
-
-
-// Start Quiz
-    // Click button
-startButton.addEventListener("click", function(){
-    // Change page elements
-    nextQuestion()
-    // Add timer element
-    startTimer()
-
-})
-
-// Quiz Questions
-function nextQuestion(){
-    console.log("This is the next question")
-
-}
-
+var questNum = 1
 
 // Timer
 function startTimer(){
-    var timer = 60;
+    var timer = 5;
 
     var timeInterval = setInterval(function() {
         timerEl.textContent = timer + " seconds remaining";
         timer--;
     
-        if (timer === 0) {
-          timerEl.textContent = "";
-          clearInterval(timeInterval);
+        if (timer === 0 || questNum === 6) {
+          highScoreScreen()
         }
     
       }, 1000);
 
 }
 
+// Start Quiz
+
+startButton.addEventListener("click", function(){
+    startButton.remove()
+    nextQuestion()
+    startTimer()
+})
+
+// Quiz Questions
+function nextQuestion(){
+
+    currentQuestionNumber.textContent = "Question #" + questNum;
+    currentQuestion.textContent = " ";
+    
+    
+
+}
+
+
 
 // High Score
+function highScoreScreen(){
+    timerEl.textContent = "";
+    clearInterval(timeInterval);
+    currentQuestionNumber.textContent = "All done!"
+    currentQuestion.textContent= "Nice Job! Enter your initials"
+          
+    var textForm = document.createElement("input")
+    textForm.setAttribute("type", "text")
+    document.body.append(textForm)
+
+    
+}
