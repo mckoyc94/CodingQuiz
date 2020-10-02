f4f8d922ec899ef185de34badcf1a2770e259b67
Cmac <mckoyc@gmail.com>
2020-10-01 22:04:24 -0400

    got button clicks to work

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ var startButton = document.getElementById("start")
 var timerEl = document.getElementById("countDown")
 var questNum = 0
 
-var buttonSection = document.createElement("div")
-    var buttonA = document.createElement("button")
-    var buttonB = document.createElement("button")
-    var buttonC = document.createElement("button")
-    var buttonD = document.createElement("button")
 
 // Quiz Questions
 var questArray = [
@@ -24,7 +19,7 @@ var questArray = [
         "answer2" : "<script>",
         "answer3" : "<javascript>",
         "answer4" : "<js>",
-        "correctAnswer" : "answer2"
+        "correctAnswer" : "<script>"
     },
     // Question 2
     {
@@ -33,7 +28,7 @@ var questArray = [
         "answer2" : "getChar()",
         "answer3" : "charAt()",
         "answer4" : "characterAt()",
-        "correctAnswer" : "answer3"
+        "correctAnswer" : "charAt()"
     },
     // Question 3
     {
@@ -42,7 +37,7 @@ var questArray = [
         "answer2" : "<thick>",
         "answer3" : "<big>",
         "answer4" : "<strong>",
-        "correctAnswer" : "answer4"
+        "correctAnswer" : "<strong>"
     },
     // Question 4
     {
@@ -51,7 +46,7 @@ var questArray = [
         "answer2" : "<merge>",
         "answer3" : "<attach>",
         "answer4" : "<bound>",
-        "correctAnswer" : "answer1"
+        "correctAnswer" : "<link>"
     },
     // Question 5
     {
@@ -60,18 +55,21 @@ var questArray = [
         "answer2" : "stringMaker()",
         "answer3" : "toString()",
         "answer4" : "subString()",
-        "correctAnswer" : "answer3"
+        "correctAnswer" : "toString()"
     }
 
 ]
-
-//Answer Key
-questArray[1].answer2
-questArray[2].answer3 
-questArray[3].answer4 
-questArray[4].answer1 
-questArray[5].answer3 
-
+ 
+//Button Variables
+var buttonSection = document.createElement("div")
+    var buttonA = document.createElement("button")
+    buttonA.setAttribute("id","button-A")
+    var buttonB = document.createElement("button")
+    buttonB.setAttribute("id", "button-B")
+    var buttonC = document.createElement("button")
+    buttonC.setAttribute("id", "button-C")
+    var buttonD = document.createElement("button")
+    buttonD.setAttribute("id", "button-D")
 
 // Timer
 function startTimer(){
@@ -110,19 +108,65 @@ startButton.addEventListener("click", function(){
 
 // Generating with Next Question
 function nextQuestion(){
-    var buttonEl = document.querySelector("button")
-    questNum++
     
+    questNum++
     currentQuestionNumber.textContent = "Question #" + questNum;
+    
+    //Buttons
     currentQuestion.textContent = questArray[questNum].text;
     buttonA.textContent = questArray[questNum].answer1
+    document.getElementById("button-A").setAttribute('value', questArray[questNum].answer1)
     buttonB.textContent = questArray[questNum].answer2
+    document.getElementById("button-B").setAttribute('value', questArray[questNum].answer2)
     buttonC.textContent = questArray[questNum].answer3
+    document.getElementById("button-C").setAttribute('value', questArray[questNum].answer3)
     buttonD.textContent = questArray[questNum].answer4
-  
-    buttonB.addEventListener('click', function(){
+    document.getElementById("button-D").setAttribute('value', questArray[questNum].answer4)
+    
+    // Button Presses
+    buttonA.addEventListener('click', function(event){
         console.log("Button clicked")
-        console.log(buttonEl)
+        console.log("event value: ", event.target.value)
+        if (event.target.value === questArray[questNum].correctAnswer){
+            console.log("it worked")
+            nextQuestion()
+        } else {
+            console.log("it didn't work")
+            nextQuestion()
+        }
+    })
+    buttonB.addEventListener('click', function(event){
+        console.log("Button clicked")
+        console.log("event value: ", event.target.value)
+        if (event.target.value === questArray[questNum].correctAnswer){
+            console.log("it worked")
+            nextQuestion()
+        } else {
+            console.log("it didn't work")
+            nextQuestion()
+        }
+    })
+    buttonC.addEventListener('click', function(event){
+        console.log("Button clicked")
+        console.log("event value: ", event.target.value)
+        if (event.target.value === questArray[questNum].correctAnswer){
+            console.log("it worked")
+            nextQuestion()
+        } else {
+            console.log("it didn't work")
+            nextQuestion()
+        }
+    })
+    buttonD.addEventListener('click', function(event){
+        console.log("Button clicked")
+        console.log("event value: ", event.target.value)
+        if (event.target.value === questArray[questNum].correctAnswer){
+            console.log("it worked")
+            nextQuestion()
+        } else {
+            console.log("it didn't work")
+            nextQuestion()
+        }
     })
 
 
